4db196f5692489ad3ba7fa4fb10f904e640ffe98
theaux <theauxperso@gmail.com>
2023-11-07 20:10:38 +0100

    🐛 Functions in .h file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tbabou <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 17:31:33 by tbabou            #+#    #+#             */
-/*   Updated: 2023/11/07 15:06:53 by tbabou           ###   ########.fr       */
+/*   Updated: 2023/11/07 16:21:20 by tbabou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int		ft_atoi(char *str) void *memset(void *str, int c, size_t n);
+char	*ft_toupper(char *str);
+int		ft_atoi(char *str);
+void	*ft_memmove(void *dst, const void *src, size_t n);
+void	*ft_memset(void *str, int c, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 #endif
