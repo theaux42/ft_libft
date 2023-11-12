afbb0c80506005a7b2a7964255f38b2ab78ac237
theaux <theaux@air-de-theaux.home>
2023-11-12 18:36:58 +0100

    :hammer: updated to latest functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tbabou <tbabou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 17:31:33 by tbabou            #+#    #+#             */
-/*   Updated: 2023/11/09 16:11:47 by tbabou           ###   ########.fr       */
+/*   Updated: 2023/11/12 02:48:17 by tbabou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,26 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-char	*ft_toupper(char *str);
+char	*ft_strdup(const char *s1);
 char	*ft_strchr(const char *s, int c);
-int		ft_tolower(int);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int		ft_tolower(int c);
 int		ft_toupper(int c);
-int		ft_atoi(char *str);
 int		ft_isspace(int c);
 int		ft_isalnum(int c);
 int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
+int		ft_intlen(int nbr);
+int		ft_atoi(char *str);
 int		ft_strlen(char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
 void	*ft_bzero(void *b, size_t len);
-void	*ft_memmove(void *dst, const void *src, size_t n);
+void	*ft_calloc(size_t count, size_t size);
 void	*ft_memset(void *str, int c, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t n);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif
