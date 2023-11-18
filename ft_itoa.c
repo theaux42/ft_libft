8f1b4711b215a7a92cf342d1c0675d621a11595f
theaux <theaux@air-de-theaux.home>
2023-11-18 02:00:14 +0100

    🔨 First try

--- ft_itoa.c
+++ ft_itoa.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tbabou <tbabou@42.fr>                      +#+  +:+       +#+        */
+/*   By: tbabou <tbabou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 18:44:29 by tbabou            #+#    #+#             */
-/*   Updated: 2023/11/16 18:00:51 by tbabou           ###   ########.fr       */
+/*   Updated: 2023/11/17 21:09:02 by tbabou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	malloc_len(int n)
 {
 	int	nlen;
 
-	nlen = ft_intlen(n);
+	nlen = ft_intlen(n) - 2;
 	if (n < 0)
 		return (sizeof(char) * nlen + 2);
 	return (sizeof(char) * nlen + 1);
@@ -26,20 +26,19 @@ int	malloc_len(int n)
 char	*ft_itoa(int n)
 {
 	char	*str;
-	int		i;
 	int		j;
 
-	i = 0;
 	j = ft_intlen(n) - 1;
 	str = malloc(malloc_len(n));
 	if (!str)
 		return (NULL);
 	if (n == -2147483648)
-		return ("-2147483648");
+		return (ft_strdup("-2147483648"));
+	if (n == 0)
+		str[0] = '0';
 	if (n < 0)
 	{
-		str[i] = '-';
-		i++;
+		str[0] = '-';
 		n = -n;
 	}
 	while (n)
@@ -48,5 +47,6 @@ char	*ft_itoa(int n)
 		n = n / 10;
 		j--;
 	}
+	str[ft_strlen(str) + 1] = '\0';
 	return (str);
 }
