a544dc794320c062b8da9b09ea7b5bcf324faa52
Theaux <theauxperso@gmail.com>
2023-11-29 07:56:50 +0100

    ✨ Fixed for bazaluga's tester

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: tbabou <tbabou@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 21:39:03 by tbabou            #+#    #+#             */
-/*   Updated: 2023/11/23 23:02:59 by tbabou           ###   ########.fr       */
+/*   Updated: 2023/11/29 07:55:42 by tbabou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,19 +48,17 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		start;
 	int		end;
-	char	*newstr;
+	char	*new;
 
-	if (s1 == NULL)
+	if (!s1 || !set)
 		return (NULL);
-	if (set == NULL)
-		return (ft_strdup(s1));
 	start = ft_getstart(s1, set);
 	end = ft_getend(s1, set);
 	if (start >= end)
 		return (ft_strdup(""));
-	newstr = (char *)malloc(sizeof(char) * (end - start + 1));
-	if (newstr == NULL)
+	new = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (new == NULL)
 		return (NULL);
-	ft_strlcpy(newstr, s1 + start, end - start + 1);
-	return (newstr);
+	ft_strlcpy(new, s1 + start, end - start + 1);
+	return (new);
 }
