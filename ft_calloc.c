746fd08f852ed8a5a546a9bf45dcde41bc55839c
Theaux (42) <tbabou@made-f0Dr9s10.clusters.42paris.fr>
2023-12-11 15:27:11 +0100

    :hammer: work ?

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,22 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tbabou <tbabou@42.fr>                      +#+  +:+       +#+        */
+/*   By: tbabou <tbabou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 02:31:57 by tbabou            #+#    #+#             */
-/*   Updated: 2023/11/29 07:32:15 by tbabou           ###   ########.fr       */
+/*   Updated: 2023/12/11 15:26:04 by tbabou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
-void	*ft_calloc(size_t count, size_t size)
+static void	s_zero(void *s, size_t n)
 {
-	void	*mem;
+	while (n--)
+		*(unsigned char *)s++ = 0;
+}
 
-	mem = malloc(count * size);
-	if (!mem)
+void	*ft_calloc(size_t count, size_t n)
+{
+	void	*memory;
+
+	if (count && n && count > (UINT_MAX / n))
 		return (NULL);
-	ft_bzero(mem, count * size);
-	return (mem);
+	memory = malloc(count * n);
+	if (!memory)
+		return (NULL);
+	s_zero(memory, count * n);
+	return (memory);
 }
